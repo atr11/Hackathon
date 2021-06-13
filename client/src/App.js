f7b5106e631f5b7055b00cb3735cf25b9e5499e9
atr11_Art Treleaven <artt@shaw.ca>
2021-06-13 16:04:33 -0600

    Fetch location descriptions into game

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
-import './App.css';
-import GameplayMap from './components/gameplayMap/GameplayMap';
-
+import './App.css'
+import React, { useState, useEffect } from 'react'
+import GameplayMap from './components/gameplayMap/GameplayMap'
+import DisplayText from './components/DisplayText'
 
 function App () {
+
+  // Define State Variables
+  const [locationList, setLocationList] = useState ([])
+  const [currentLocation,setCurrentLocation] = useState("Calgary")
+  const [itemsToMap,setItemsToMap] = useState([])
+  const [moveCounter,setMoveCounter] = useState(0)
+  
+  // On first load, retrieve all story locations from database.
+  useEffect(() => { 
+    const getLocations = async () => {
+      let response = await fetch('/api/location')
+      let data = await response.json()
+      setLocationList(data)
+    }
+    getLocations()
+  }, [])
+
   return(
     <div className="main-background">
         <div className="map-and-story">
@@ -19,14 +37,17 @@ function App () {
           <GameplayMap />
         </div>
         <div>
-            <button> Start </button>
             <div>
-                <p> Story text can go here</p>
+              <DisplayText locationList = {locationList} currentLocation = {currentLocation} />
             </div>
+            <div>
+              <ContinueStory itemsToMap = {itemsToMap}/>
+            </div>
+            <button> Start Over! </button>
         </div>
     </div>    
   )  
 }
 
-export default App;
+export default App
 
---
+++ client/src/components/DisplayText.js
@@ -0,0 +1,23 @@
+import React from "react"
+import "./DisplayText.css"
+
+const DisplayText = ( {locationList, currentLocation} ) => {
+
+    // Retrieve 'description' of location provided
+
+    // let storyText = locationList[0].description
+    let storyText = ""
+    let index = 0
+    index = locationList.map(function(e) { return e.city; }).indexOf('Mexico City')
+    if (index > -1) {
+        storyText = locationList[index].description
+      }
+
+    return(
+        <div className="DisplayText">
+            {storyText}
+        </div>
+    )
+}
+
+export default DisplayText
